4ace688ae8c20cd73d3f1051fee094b4431b0228
w3dog1 <go.gett2r@gmail.com>
2021-10-21 02:36:56 +0900

    add frontend setup

--- src/server.js
+++ src/server.js
@@ -2,8 +2,13 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+app.set('view engine', 'pug');
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+//route handler
 
-app.listen(3000);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
 
-//실험합니다 커밋 되는지
